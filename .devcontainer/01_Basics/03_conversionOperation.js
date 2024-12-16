538fd279d34fb5e4947ec2b3050a3270f1862e5f
Harshit Mishra <154236143+HarshitMishra12@users.noreply.github.com>
2024-12-16 20:57:34 +0000

    Conversion of dataTypes

---
+++ .devcontainer/01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+let isoLoggedIn = 1
+
+let boooleanIsoLoggedIn = Boolean(isoLoggedIn)
+//console.log(boooleanIsoLoggedIn);
+
+//1 => true; 0 => false
+// "" => false
+//"Harshit" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
