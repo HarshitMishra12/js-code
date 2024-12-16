93e055d978870ffbb52d3d647a3d6718eab03965
Harshit Mishra <154236143+HarshitMishra12@users.noreply.github.com>
2024-12-16 15:23:55 +0000

    dataTypes

---
+++ .devcontainer/01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+// aleart(3+3) // we are using nodejs,not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+    console .log("Harshit")
+
+
+    let name ="Harshit"// String
+    let age = 20// Char
+    let isLoggedIn = false// Boolean
+    
+    // number => 2 to power 53
+    // bigint (mostly used in trading apps or stock market or facbook)
+    // string => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    // Object
+
+    console.log(typeof age);// to see the type of anything
