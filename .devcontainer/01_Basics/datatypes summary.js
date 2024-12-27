be8fe6d6265d761189720b3b27ec404d98b52cca
Harshit Mishra <154236143+HarshitMishra12@users.noreply.github.com>
2024-12-27 17:43:41 +0000

    datatype summary

---
+++ .devcontainer/01_Basics/datatypes summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+// In javascript we never define languages
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const = anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1234567898987654321n
+
+
+//Reference (Non Primiive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", " naagraj", "doga"];
+let myObj = {
+    name: "harshit",
+    age: 20,
+}
+
+function(){}
